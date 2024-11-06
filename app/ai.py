d79d53f41d1f3d45505c35b4e8cd7877f8791bf6
Aaron Chen <dev.kst.aaron@gmail.com>
2024-11-06 22:57:59 +0900

    init: v0.1.0

---
+++ app/ai.py
@@ -0,0 +1,60 @@
+from openai import OpenAI
+
+class Waddy():
+    def __init__(self, api_key):
+        self.client = OpenAI(
+            api_key=api_key,
+        )
+
+    def basic_run(self, messages, assistant_id):
+        try:
+            stream = self.client.beta.threads.create_and_run(
+                assistant_id=assistant_id, thread={"messages": messages}, stream=True
+            )
+            for event in stream:
+                if (
+                    event.event == "thread.created"
+                    and event.data.id
+                ):
+                    print('created thread:', event.data.id)
+                elif event.event == "thread.message.delta" and event.data.delta.content:
+                    if event.data.delta.content[0].type == "text":
+                        current_content = event.data.delta.content[0].text.value or ""
+                        yield f"data: {current_content}\n\n"
+                    else:
+                        print(event.data.delta.content[0])
+                elif event.event == "thread.run.completed":
+                    print("\n----")
+                    print(event.data.usage) # Total tokens
+                    yield f"data: {str(event.data.usage)}\n\n"
+        except Exception as e:
+            print("Error in creating thread and run from openAI:", str(e))
+            raise e
+
+
+    def _get_thread_messages(self, thread_id: str, after="", messages=[]):
+        try:
+            message_list = self.client.beta.threads.messages.list(thread_id, after=after)
+            for message in message_list.data:
+                messages.append(message)
+            if message_list.has_more is True and message_list.last_id:
+                self._get_thread_messages(thread_id, after=message_list.last_id, messages=messages)
+            else:
+                return messages
+        except Exception as e:
+            print("Error in getting thread details:", str(e))
+            raise e
+
+    def get_thread_details(self, thread_id):
+        try:
+            print('getting thread details')
+            thread_details = self.client.beta.threads.retrieve(thread_id)
+            messages = []
+            self._get_thread_messages(thread_id, messages=messages)
+            return {
+                **thread_details.__dict__,  # Convert the Thread object to a dictionary
+                "messages": messages   # Add messages to the details
+            }
+        except Exception as e:
+            print('Error in getting message list:', str(e))
+            raise e
---
+++ app/main.py
@@ -0,0 +1,73 @@
+import logging
+import os
+import sys
+
+from dotenv import load_dotenv
+from fastapi import FastAPI, HTTPException, BackgroundTasks
+from fastapi.responses import StreamingResponse, JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from app.ai import Waddy
+
+# Ensure level is INFO or lower to see INFO messages
+logging.basicConfig(level=logging.INFO)
+# Create a named logger instance
+logger = logging.getLogger(__name__)
+
+# Setup
+origins = [
+    "*",  # TODO: set the allowed origins
+]
+load_dotenv()
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+OPEN_AI_API_KEY = os.getenv("OPEN_AI_API_KEY", "")
+OPEN_AI_ASSISTANT_ID = os.getenv("OPEN_AI_ASSISTANT_ID", "")
+
+if not len(OPEN_AI_API_KEY) or not len(OPEN_AI_ASSISTANT_ID):
+    print("Please set correct environment variables. Exiting...")
+    sys.exit(1)
+
+client = Waddy(
+    api_key=OPEN_AI_API_KEY,
+)
+
+# API Routes
+class BasicConversation(BaseModel):
+    message: str
+
+@app.post("/basic")
+def basic(payload: BasicConversation):
+    try:
+        messages = []
+        messages.append({"role": "user", "content": payload.message})
+
+        return StreamingResponse(
+            client.basic_run(
+                messages=messages,
+                assistant_id=OPEN_AI_ASSISTANT_ID
+            ),
+            media_type='text/event-stream'
+        )
+    except Exception as e:
+        print("Error in creating thread and run from openAI:", str(e))
+        raise HTTPException(500, str(e))
+
+# testing thread id: thread_DWD5dVCcK1mhgh62fpWvcXdE
+@app.get('/threads/{thread_id}')
+def thread_details(thread_id: str):
+    try:
+        return {
+            "err": 0,
+            "status": "success",
+            "data": client.get_thread_details(thread_id=thread_id)
+        }
+    except Exception as e:
+        print("Error in creating thread and run from openAI:", str(e))
+        raise HTTPException(500, str(e))
